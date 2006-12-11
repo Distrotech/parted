6928477436823359850941fabd78444635752095
Otavio Salvador <otavio@ossystems.com.br>
2006-12-11 07:46:34 -0200

    Include a disk probing test

---
+++ libparted/tests/common.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+
+#include "common.h"
+
+
+char *_create_disk(const off_t size)
+{
+		char filename[] = "parted-test-XXXXXX";
+		mktemp(filename);
+
+		FILE *disk = fopen(filename, "w");
+		off_t total_size = size * 1024 * 1024; /* Mb */
+
+		fseek(disk, total_size, SEEK_SET);
+		fwrite("", sizeof(char), sizeof(char), disk);
+		fclose(disk);
+
+		return strdup(filename);
+}
---
+++ libparted/tests/common.h
@@ -0,0 +1,6 @@
+/* Create an empty disk image
+ *
+ * filename: file (with full path) where to write the disk image
+ *     size: size of disk image (megabytes)
+ */
+char *_create_disk(const off_t size);
