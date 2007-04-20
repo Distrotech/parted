6f04f317301b505079f492d57977f46c1e745ed2
Debarshi Ray <rishi@gnu.org>
2007-04-21 00:59:15 +0530

    [tests] New test for probing of disk labels. (cherry picked from commit b40a7a0976280fc861d5afa7563b28f561818b55)

--- libparted/tests/label.c
+++ libparted/tests/label.c
@@ -52,6 +52,38 @@ START_TEST (test_create_label)
 }
 END_TEST
 
+/* TEST: Probe the disk label of a loop device. */
+START_TEST (test_probe_label)
+{
+        PedDevice* dev = ped_device_get (temporary_disk);
+        if (dev == NULL)
+                return;
+
+        PedDiskType* probed;
+        PedDiskType* type;
+        PedDisk* disk;
+
+        for (type = ped_disk_type_get_next (NULL); type;
+             type = ped_disk_type_get_next (type)) {
+                if (!_implemented_disk_label (type->name))
+                        continue;
+
+                disk = _create_disk_label (dev, type);
+                ped_disk_destroy (disk);
+
+                /* Try to probe the disk label. */
+                probed = ped_disk_probe (dev);
+                fail_if (!probed,
+                         "Failed to probe the just created label of type: %s",
+                         type->name);
+                fail_if (strcmp (probed->name, type->name) != 0,
+                         "Probe returned label of type: %s as type: %s",
+                         type->name, probed->name);
+        }
+        ped_device_destroy (dev);
+}
+END_TEST
+
 /* TEST: Clone the disk label of a loop device. */
 START_TEST (test_clone_label)
 {
@@ -89,6 +121,7 @@ main (void)
         int number_failed;
         Suite* suite = suite_create ("Disk Label");
         TCase* tcase_basic = tcase_create ("Create");
+        TCase* tcase_probe = tcase_create ("Probe");
         TCase* tcase_clone = tcase_create ("Clone");
 
         /* Fail when an exception is raised */
@@ -99,6 +132,12 @@ main (void)
         /* Disable timeout for this test */
         tcase_set_timeout (tcase_basic, 0);
         suite_add_tcase (suite, tcase_basic);
+
+        tcase_add_checked_fixture (tcase_probe, create_disk, destroy_disk);
+        tcase_add_test (tcase_probe, test_probe_label);
+        /* Disable timeout for this test. */
+        tcase_set_timeout (tcase_probe, 0);
+        suite_add_tcase (suite, tcase_probe);
 
         tcase_add_checked_fixture (tcase_clone, create_disk, destroy_disk);
         tcase_add_test (tcase_clone, test_clone_label);
