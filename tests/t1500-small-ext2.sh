b3ce056b4e3b2835872255f3844e390c52bd197b
Jim Meyering <jim@meyering.net>
2007-05-18 15:52:00 +0200

    mkpartfs ext2 2 10 would erroneously report "file system too small" for some small (single-group) partitions.  It would also fail to report "file system too small" in some cases, and instead continue on to into inode-allocation code where it'd report "File system full!" * libparted/fs/ext2/ext2_mkfs.c (compute_block_counts): New function, factored out of... (ext2_mkfs): ...here.  Call compute_block_counts rather than open-coded it.  When decrementing numblocks, call compute_block_counts again, to recompute all of the derived values. Require at least 14 free blocks in a 16-inode-per-group partition. * tests/t1500-small-ext2.sh: New file.  Test for the above. * tests/Makefile.am (TESTS): Add t1500-small-ext2.sh. (cherry picked from commit 808ce3edfc83c0e69d3cef6c249a6b578efe60fa)

---
+++ tests/t1500-small-ext2.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# Copyright (C) 2007 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+test_description='Create very small ext2 file systems.'
+
+. ./init.sh
+
+dev=loop-file
+test_expect_success \
+    'setup' '
+    dd if=/dev/zero of=$dev bs=10M count=1 2> /dev/null &&
+    parted -s $dev mklabel msdos'
+
+test_expect_failure \
+    'try to create an ext2 partition that is one byte too small' '
+    parted -s $dev mkpartfs primary ext2 10KB 29695B > out 2>&1'
+
+test_expect_success \
+    'check for expected diagnostic' '
+    echo Error: File system too small for ext2. > exp &&
+    $compare out exp'
+
+test_expect_success \
+    'create the smallest ext2 partition' '
+    parted -s $dev mkpartfs primary ext2 10KB 29696B > out 2>&1
+    $compare out /dev/null'
+
+# Restore $dev to initial state by writing 1KB of zeroes at the beginning.
+# Then relabel.
+test_expect_success \
+    'setup' '
+    dd if=/dev/zero of=$dev bs=1K count=1 conv=notrunc 2> /dev/null &&
+    parted -s $dev mklabel msdos'
+
+test_expect_success \
+    'create another ext2 file system (this would fail for parted-1.8.7)' '
+    parted -s $dev mkpartfs primary ext2 2 10 > out 2>&1'
+test_expect_success 'expect no output' '$compare out /dev/null'
+
+test_expect_success \
+    'create a smaller one; this would succeed for parted-1.8.7' '
+    dd if=/dev/zero of=$dev bs=1K count=1 conv=notrunc 2> /dev/null &&
+    parted -s $dev mklabel msdos &&
+    parted -s $dev mkpartfs primary ext2 2 9 > out 2>&1'
+test_expect_success 'expect no output' '$compare out /dev/null'
+
+test_done
