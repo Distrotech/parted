07a13495995bd0695f73e48be9d89c36df42d1fa
Jim Meyering <jim@meyering.net>
2007-05-23 22:28:53 +0200

    Don't fail all tests when "." lacks O_DIRECT support.
    I often build tools on a tmpfs file system (it's faster), and
    found that parted tests always failed there.  That's because it tries
    to open the "device" (a file) with O_DIRECT, and at least the linux tmpfs
    driver always fails with EINVAL in that case.
    
    So here's a patch that makes it work.
    Since the test may require writing in a directory like /tmp,
    to which others typically have write access, it is particularly
    careful about security (see the mkdtemp script below), in case
    "make check" is run by e.g., root.
    
    Don't fail all tests when "." lacks O_DIRECT support.
    Before, running "make check" on a file system that doesn't support
    O_DIRECT (e.g. tmpfs), would always fail.  Now, it works, as long as
    the test machinery can find a writable directory in which open with
    O_DIRECT *does* work.
    * m4/o-direct.m4: New file.  Find a directory/FS with O_DIRECT support.
    * configure.ac: Use the new macro.
    * libparted/tests/t1000-label.sh: New file.  Wrap the binary, so
    it can take advantage of the code that finds O_DIRECT supporting FS.
    * tests/mkdtemp: New file.  Required, since when running tests as
    root, we may have to create a temporary directory in a directory
    like /tmp that's writable by others.
    * tests/Makefile.am (EXTRA_DIST): Add mkdtemp.
    * tests/test-lib.sh: When creating test subdir, and setting up "trap",
    use the directory specified in $PARTED_USABLE_TEST_DIR.
    Don't set PATH here.  Now, that's done via the generated, and always-
    sourced, init.sh.  As a result, invoke parted via its full file name.
    (cherry picked from commit 873251291e04dec0f7ab0d5b7d65809981ed95b4)

---
+++ libparted/tests/t1000-label.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Copyright (C) 2007 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+test_description='run the label unit tests in a directory supporting O_DIRECT'
+# This wrapper around the ./label binary is used to find a directory
+# in which one can open a file with the O_DIRECT flag.
+
+. ./init.sh
+
+test_expect_success \
+    'run the actual tests' 'label'
+
+test_done
