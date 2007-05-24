c7d6aaf8fc01f42a58dacbccfeac1f2b474321a3
Jim Meyering <jim@meyering.net>
2007-05-24 20:00:22 +0200

    Diagnose "ext2 FS too small" rather than triggering an assertion. * libparted/fs/ext2/ext2_mkfs.c (ext2_mkfs): An overlapping ext2 partition request could still lead to a bug: constraint-resolution code would produce a single-sector candidate "range", and that would cause the ext2 fs-creation code to misbehave.  Now, it properly detects and reports the FS as being too small. * tests/t3000-constraints.sh: New test for the above. * tests/Makefile.am (TESTS): Add t3000-constraints.sh. (cherry picked from commit 32c069f0b2404cbc75eff3f729207bf9193979d0)

---
+++ tests/t3000-constraints.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Copyright (C) 2007 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+test_description="exercise Parted's constraint-management code"
+
+. ./init.sh
+
+# FIXME: move this to test-lib.sh, and use it in t0000*.sh
+emit_superuser_warning()
+{
+  uid=`id -u` || uid=1
+  test "$uid" != 0 &&
+    echo 'WARNING: You are not superuser.  Watch out for permissions.'
+}
+
+dev=loop-file
+N=2
+t=ext2
+
+test_expect_success \
+    "setup: label and create a small $t partition" \
+    'dd if=/dev/zero of=$dev bs=${N}M count=1 2>/dev/null &&
+     { echo y; echo c; } > in &&
+     { emit_superuser_warning
+       echo "Warning: You requested a partition from 1000kB to 2000kB."
+       echo "The closest location we can manage is 15.9kB to 15.9kB." \
+	    " Is this still acceptable to you?"
+       echo "Yes/No? y"
+       echo "Error: File system too small for ext2."; } > exp &&
+     parted -s $dev mklabel msdos &&
+     parted -s $dev mkpartfs primary $t 1 $N'
+
+# Before parted-1.9, this would fail with a buffer overrun
+# leading to a segfault.
+test_expect_failure \
+    'try to create another partition in the same place' \
+    'parted ---pretend-input-tty $dev mkpartfs primary $t 1 $N <in >out 2>&1'
+
+test_expect_success \
+    'normalize the actual output' \
+    'sed "s,   *,,;s, $,," out > o2 && mv -f o2 out'
+
+test_expect_success 'check for expected output' '$compare out exp'
+
+test_done
