c1f7c819ebf68d9c92c8f333e53fef672dc820fb
David Cantrell <dcantrel@mortise.boston.redhat.com>
2007-07-23 13:54:23 -0400

    Updated GPL license boilerplate.

--- doc/gendocs.sh
+++ doc/gendocs.sh
@@ -3,11 +3,11 @@
 #   mentioned in maintain.texi.  See the help message below for usage details.
 # $Id: gendocs.sh,v 1.16 2005/05/15 00:00:08 karl Exp $
 # 
-# Copyright (C) 2003, 2004, 2005 Free Software Foundation, Inc.
+# Copyright (C) 2003, 2004, 2005, 2006, 2007 Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
+# the Free Software Foundation; either version 3, or (at your option)
 # any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -16,9 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, you can either send email to this
-# program's maintainer or write to: The Free Software Foundation,
-# Inc.; 51 Franklin Street, Fifth Floor; Boston, MA 02110-1301, USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Original author: Mohit Agarwal.
 # Send bug reports and any other correspondence to bug-texinfo@gnu.org.
--- libparted/mbr.s
+++ libparted/mbr.s
@@ -1,9 +1,9 @@
 ;   libparted - a library for manipulating disk partitions
-;   Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+;   Copyright (C) 1999, 2000, 2007 Free Software Foundation, Inc.
 ;
 ;   This program is free software; you can redistribute it and/or modify
 ;   it under the terms of the GNU General Public License as published by
-;   the Free Software Foundation; either version 2 of the License, or
+;   the Free Software Foundation; either version 3 of the License, or
 ;   (at your option) any later version.
 ;
 ;   This program is distributed in the hope that it will be useful,
@@ -12,8 +12,7 @@
 ;   GNU General Public License for more details.
 ;
 ;   You should have received a copy of the GNU General Public License
-;   along with this program; if not, write to the Free Software
-;   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+;   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ; NOTE: I build this with:
 ;	$ as86 -b /dev/stdout mbr.s | hexdump -e '8/1 "0x%02x, " "\n"'
