Distrotech/parted

commit f6bd20573e3ecfb63f62d88c52a0870fb8851b59

fix computation of largest partition number
* libparted/arch/linux.c (_disk_sync_part_table): s/PED_MAX/PED_MIN/
(_dm_reread_part_table): Likewise.
Patch by Petr Uzel (tiny change).
Details in http://thread.gmane.org/gmane.comp.gnu.parted.devel/2213

libparted/arch/linux.c
@@ -2212,7 +2212,7 @@ static int
 _disk_sync_part_table (PedDisk* disk)
 {
         int     i;
-        int     last = PED_MAX (ped_disk_get_last_partition_num (disk), 16);
+        int     last = PED_MIN (ped_disk_get_last_partition_num (disk), 16);
         int*    rets = ped_malloc(sizeof(int) * last);
         int*    errnums = ped_malloc(sizeof(int) * last);
         int     ret = 1;
@@ -2433,7 +2433,7 @@ static int
 _dm_reread_part_table (PedDisk* disk)
 {
         int     rc = 1;
-        int     last = PED_MAX (ped_disk_get_last_partition_num (disk), 16);
+        int     last = PED_MIN (ped_disk_get_last_partition_num (disk), 16);
         int     i;
 
         sync();

commit 1742b051d78493b90bcb801e68a2be0277bcf72f

don't exhaust virtual memory for mklabel on a loopback device
Report and analysis by Soren Hansen <soren@canonical.com>:
"""When ped_disk_get_last_partition_num(disk) fails (which it does if
disk is a loopback device), it returns -1. This in turn would cause
_disk_sync_part_table and _dm_reread_part_table to attempt to malloc
a sizeof(int)*-1, which instantly causes a 'Fatal: Out of memory'."""
* libparted/arch/linux.c (_disk_sync_part_table): Handle the case in
which ped_disk_get_last_partition_num(disk) fails.
(_dm_reread_part_table): Likewise.
This bug was introduced by f6bd20573e3ecfb63f62d88c52a0870fb8851b59.

libparted/arch/linux.c
@@ -2211,11 +2211,15 @@ _blkpg_remove_partition (PedDisk* disk, int n)
 static int
 _disk_sync_part_table (PedDisk* disk)
 {
-        int     i;
-        int     last = PED_MIN (ped_disk_get_last_partition_num (disk), 16);
+        int largest_partnum = ped_disk_get_last_partition_num (disk);
+        if (largest_partnum <= 0)
+          return 1;
+
+        int     last = PED_MIN (largest_partnum, 16);
         int*    rets = ped_malloc(sizeof(int) * last);
         int*    errnums = ped_malloc(sizeof(int) * last);
         int     ret = 1;
+        int     i;
 
         for (i = 1; i <= last; i++) {
                 rets[i - 1] = _blkpg_remove_partition (disk, i);
@@ -2433,8 +2437,12 @@ err:
 static int
 _dm_reread_part_table (PedDisk* disk)
 {
+        int largest_partnum = ped_disk_get_last_partition_num (disk);
+        if (largest_partnum <= 0)
+          return 1;
+
         int     rc = 1;
-        int     last = PED_MIN (ped_disk_get_last_partition_num (disk), 16);
+        int     last = PED_MIN (largest_partnum, 16);
         int     i;
 
         sync();