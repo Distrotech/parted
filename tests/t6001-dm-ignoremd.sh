ebb2720c5dc4ae50f41b137cf2a2826e4ccbacce
Joel Granados Moreno <jgranado@redhat.com>
2009-06-10 19:05:53 +0200

    tests: simplify t6001-dm-ignoremd.sh
    * tests/t6001-dm-ignoremd.sh (md_dev_create_): Use functions in
    test-util.sh for mdadm commands.  Move mddev_ to the top of the
    file.  Remove unneeded comment.

--- tests/t6001-dm-ignoremd.sh
+++ tests/t6001-dm-ignoremd.sh
@@ -18,11 +18,14 @@
 test_description='Ignore devices that start with md from /sys/block.'
 
 privileges_required_=1
-# This will setup private /dev and /etc
 device_mapper_required_=1
 
 : ${srcdir=.}
 . $srcdir/test-lib.sh
+
+require_mdadm_
+
+mddev_=
 
 test "x$ENABLE_DEVICE_MAPPER" = xyes ||
   {
@@ -38,36 +41,10 @@ test -d /sys/block ||
     exit
   }
 
-which mdadm > /dev/null 2>&1 ||
-  {
-    say "skipping $0: could not find mdadm executable"
-    test_done
-    exit
-  }
-
-# The md device.
-mddev_=
-
 cleanup_() {
     mdadm --stop $mddev_ > /dev/null 2>&1
     test -n "$d1" && losetup -d "$d1"
     rm -f "$f1";
-}
-
-md_dev_create_()
-{
-  lo_dev=$1
-  mdd=$G_dev_/md0
-  for i in 0 1 2 3 4 5 6 7 8 9 ; do
-      mdd=$G_dev_/md$i
-      mdadm  --create --force $mdd --level=linear --raid-devices=1 $lo_dev > /dev/null 2>&1 \
-      && break
-
-      if [ $i -eq 9 ]; then echo $mdd ; return 1 ; fi
-  done
-
-  echo $mdd
-  return 0
 }
 
 test_expect_success \
@@ -76,7 +53,7 @@ test_expect_success \
 
 test_expect_success \
     'setup: create md# device' \
-    'mddev_=$(md_dev_create_ "$d1")'
+    'mddev_=$(mdadm_create_linear_device_ "$d1")'
 
 test_expect_failure \
     'grep for the created md device' \
