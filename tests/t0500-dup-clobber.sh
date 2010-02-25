d462d59f8c290edcc053b6677143d745c4d51d67
Jim Meyering <meyering@redhat.com>
2010-02-25 15:05:46 +0100

    tests: wrap the dup-clobber test in a script, ...
    so that its artifacts are created in the usual temporary subdir
    * tests/Makefile.am (TESTS): Remove dup-clobber, and instead...
    Add t0500-dup-clobber.sh.
    * tests/t0500-dup-clobber.sh: New script.

---
+++ tests/t0500-dup-clobber.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Drive the dup-clobber program.
+
+# Copyright (C) 2009-2010 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if test "$VERBOSE" = yes; then
+  set -x
+  parted --version
+fi
+
+: ${srcdir=.}
+. $srcdir/t-lib.sh
+
+PATH="..:$PATH"
+export PATH
+
+fail=0
+dup-clobber || fail=1
+
+Exit $fail
