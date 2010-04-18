910d134c830a81c191e15456bc2df37d6f8b2616
Jim Meyering <meyering@redhat.com>
2010-04-18 11:15:45 +0200

    tests: remove partition explicitly before mklabel, to avoid new failure
    * tests/t3000-resize-fs.sh: Before, mklabel was sufficient to clear
    the partition table in preparation for the next iteration in this test.
    However, with recent BLKRRPART/BLKPG changes that "mklabel" would evoke
    a warning.  Remove the sole partition to avoid that.

--- tests/t3000-resize-fs.sh
+++ tests/t3000-resize-fs.sh
@@ -111,6 +111,9 @@ for fs_type in hfs+ fat32; do
   printf "1:$start:$new_end:530082s:$fs_type:primary:$ms;\n" > exp || fail=1
   compare out exp || fail=1
 
+  # Remove the partition explicitly, so that mklabel doesn't evoke a warning.
+  parted -s $dev rm 1 || fail=1
+
   # Create a clean partition table for the next iteration.
   parted -s $dev mklabel gpt > out 2>&1 || fail=1
   # expect no output
