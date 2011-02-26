e9e3ad6328c58512d6a3ede592327e89f6146b82
Jim Meyering <meyering@redhat.com>
2011-02-26 07:24:13 +0100

    tests: prepare t3310-flags to check gpt boolean flags, too
    * tests/t3310-flags.sh: Put a loop around the body of the test,
    and factor out the partition table name.

--- tests/t3310-flags.sh
+++ tests/t3310-flags.sh
@@ -20,37 +20,45 @@
 ss=$sector_size_
 dev=dev-file
 
-# Extract all msdos flag names from the texinfo documentation.
-msdos_flags=$(sed -n '/^@node set/,/^@node/p' "$abs_top_srcdir/doc/parted.texi" \
-  | perl -00 -ne '/^\@item (\w+).*MS-DOS/s and print lc($1), "\n"')
+for table_type in msdos; do
 
-n_sectors=5000
-dd if=/dev/null of=$dev bs=$ss seek=$n_sectors || fail=1
+  # Extract flag names of type $table_type from the texinfo documentation.
+  case $table_type in
+      msdos) search_term=MS-DOS;;
+  esac
+  flags=$(sed -n '/^@node set/,/^@node/p' \
+                    "$abs_top_srcdir/doc/parted.texi" \
+                | perl -00 -ne \
+                    '/^\@item (\w+).*'"$search_term"'/s and print lc($1), "\n"')
 
-parted -s $dev mklabel msdos \
-  mkpart pri ext2 $((1*2048))s $((2*2048-1))s \
-    > out 2> err || fail=1
-compare out /dev/null || fail=1
+  n_sectors=5000
+  dd if=/dev/null of=$dev bs=$ss seek=$n_sectors || fail=1
 
-for mode in on_only on_and_off ; do
-  for flag in $msdos_flags; do
+  parted -s $dev mklabel $table_type \
+    mkpart pri ext2 $((1*2048))s $((2*2048-1))s \
+      > out 2> err || fail=1
+  compare out /dev/null || fail=1
 
-    # Exclude the supplemental flags.
-    # These are not boolean, like the others.
-    case $flag in boot|lba|hidden) continue;; esac
+  for mode in on_only on_and_off ; do
+    for flag in $flags; do
 
-    # Turn on each flag, one at a time.
-    parted -m -s $dev set 1 $flag on u s print > raw 2> err || fail=1
-    perl -nle '/^1:2048s:4095s:2048s:::(\w+);$/ and print $1' raw > out
-    compare out exp || fail=1
-    compare err /dev/null || fail=1
+      # Exclude the supplemental flags.
+      # These are not boolean, like the others.
+      case $flag in boot|lba|hidden) continue;; esac
 
-    if test $mode = on_and_off; then
-      # Turn it off
-      parted -m -s $dev set 1 $flag off u s print > raw 2> err || fail=1
-      perl -nle '/^1:2048s:4095s:2048s:::.*;$/ and print $1' raw > out
+      # Turn on each flag, one at a time.
+      parted -m -s $dev set 1 $flag on u s print > raw 2> err || fail=1
+      perl -nle '/^1:2048s:4095s:2048s:::(\w+);$/ and print $1' raw > out
+      compare out exp || fail=1
       compare err /dev/null || fail=1
-    fi
+
+      if test $mode = on_and_off; then
+        # Turn it off
+        parted -m -s $dev set 1 $flag off u s print > raw 2> err || fail=1
+        perl -nle '/^1:2048s:4095s:2048s:::.*;$/ and print $1' raw > out
+        compare err /dev/null || fail=1
+      fi
+    done
   done
 done
 
